3d5d8053abc260ab764188fd7a182cd9d26182da
aliagha <aghajani.dev@gmail.com>
2022-07-22 01:09:05 +0430

    add code for strings

--- sub-strings/substrings.rb
+++ sub-strings/substrings.rb
@@ -1,12 +1,12 @@
 def substrings(string , dictionary)
-  dictionary.reduce (Hash.new(0)) do |hash, word|
-    if string.include?(word) 
-    hash[word] += 1
-    end
+  string = string.downcase
+  str_arr = string.split(" ") 
+  str_arr.reduce (Hash.new(0)) do |hash, string|
+    dictionary.map {|text|  hash[text] += 1 if string.include?(text)} 
     hash
   end
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
